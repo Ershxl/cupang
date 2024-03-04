d79988e6e97985e76d6eb12e1c5ba74e838cc02c
huh? <145762606+Ershxl@users.noreply.github.com>
2024-03-04 14:12:15 +0700

    Add files via upload

---
+++ cupang.java
@@ -0,0 +1,11 @@
+public class cupang {
+    public static void main(String[] args) {
+         String name = "ershal;";
+         String addres;
+
+        public void eat(){
+            System.out.println("makan: "+name);
+
+    }
+    }
+}
